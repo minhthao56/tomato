f5c2be5223a8d37fe018631d7b9b0bb046032db3
minhthao56 <minthao5648@gmail.com>
2020-08-25 17:01:24 +0700

    finisn

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ImageTomato from "../assets/tomato.png";
+
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <img
+        src={ImageTomato}
+        alt=""
+        style={{
+          width: 600,
+        }}
+      />
+      <h2 style={{ fontSize: 30 }}>
+        <i>Coming Soon</i>
+      </h2>
+    </div>
+  );
+}
