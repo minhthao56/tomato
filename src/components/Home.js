03109d5cb5fc79eddb90d23df3d76e947c419010
minhthao56 <minthao5648@gmail.com>
2020-08-26 15:26:25 +0700

    change finall

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,28 +3,23 @@ import comingSoonVideo from "../assets/comming-soon.mp4";
 
 export default function Home() {
   return (
-    <div
+    <video
+      autoPlay
+      muted
+      loop
+      id="myVideo"
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        height: "100vh",
+        position: "fixed",
+        right: 0,
+        left: 0,
+        bottom: 0,
+        top: 0,
+        minHeight: "100%",
+        minWidth: "100%",
       }}
     >
-      <video
-        autoPlay
-        muted
-        loop
-        id="myVideo"
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-      >
-        <source src={comingSoonVideo} type="video/mp4" />
-        Your browser does not support HTML5 video.
-      </video>
-    </div>
+      <source src={comingSoonVideo} type="video/mp4" />
+      Your browser does not support HTML5 video.
+    </video>
   );
 }
