7b087ee3ec05ffe8fa3fac707d4e96a88fe22cd8
minhthao56 <minthao5648@gmail.com>
2020-08-26 14:20:27 +0700

    change video again

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,27 +3,22 @@ import comingSoonVideo from "../assets/comming-soon.mp4";
 
 export default function Home() {
   return (
-    <div
+    <video
+      autoPlay
+      muted
+      loop
+      id="myVideo"
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        flexDirection: "column",
+        position: "fixed",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        width: "100%",
       }}
     >
-      <video
-        autoPlay
-        muted
-        loop
-        id="myVideo"
-        style={{
-          width: "100%",
-        }}
-      >
-        <source src={comingSoonVideo} type="video/mp4" />
-        Your browser does not support HTML5 video.
-      </video>
-    </div>
+      <source src={comingSoonVideo} type="video/mp4" />
+      Your browser does not support HTML5 video.
+    </video>
   );
 }
